896497f2b58b643eaece5e3f4d00d6b831d33238
Anita Lu <anitalu@Anita-MacBook-Pro.local>
2023-10-10 01:38:57 -0700

    Updates

---
+++ precache-manifest.db090b4025209f9c8e89f58949e62c2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ee8fc01040480e03c469640be45f46",
+    "url": "/anitalu724.github.io/index.html"
+  },
+  {
+    "revision": "69a4aba05efb063448eb",
+    "url": "/anitalu724.github.io/static/css/2.9cdea9f6.chunk.css"
+  },
+  {
+    "revision": "8deaa392f535e7191fcb",
+    "url": "/anitalu724.github.io/static/css/main.5be5bf91.chunk.css"
+  },
+  {
+    "revision": "69a4aba05efb063448eb",
+    "url": "/anitalu724.github.io/static/js/2.f18c9773.chunk.js"
+  },
+  {
+    "revision": "a2a3ea03193cce795903",
+    "url": "/anitalu724.github.io/static/js/3.3795ee4d.chunk.js"
+  },
+  {
+    "revision": "8deaa392f535e7191fcb",
+    "url": "/anitalu724.github.io/static/js/main.23dec4d9.chunk.js"
+  },
+  {
+    "revision": "8a4bca5551814cb2a4b3",
+    "url": "/anitalu724.github.io/static/js/runtime~main.3dfdebd2.js"
+  }
+]);
